b0423cda1e7c14daea6b9938f384ce3adfd0bf68
Matt Cheung <matt.cheung@oocl.com>
2020-04-08 21:25:45 +0800

    pass test should_return_invalid_and_invalid_input_message

--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -7,11 +7,23 @@ public class GuessNumber {
 		String randomAnswer = "";
 		for (int count = 0; count < 4; count++) {
 			String randomDigit = String.valueOf((new Random().nextInt(10)));
-				while (randomAnswer.contains(randomDigit)) {
-					randomDigit = String.valueOf((new Random().nextInt(10)));
-				}
-				randomAnswer += randomDigit;
+			while (randomAnswer.contains(randomDigit)) {
+				randomDigit = String.valueOf((new Random().nextInt(10)));
+			}
+			randomAnswer += randomDigit;
 		}
 		return randomAnswer;
+	}
+
+	public String validateInput(String input) {
+		if (input.length() != 4)
+			return "Wrong Input，Input again";
+		for (int digit1 = 0; digit1 < input.length(); digit1++) {
+			for (int digit2 = digit1 + 1; digit2 < input.length(); digit2++) {
+				if (input.charAt(digit1) == input.charAt(digit2))
+					return "Wrong Input，Input again";
+			}
+		}
+		return "ok";
 	}
 }
--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -1,13 +1,27 @@
 package com.oocl;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class GuessNumberTest {
+
 	@Test
 	public void should_return_random_answer() {
 		GuessNumber guessNumber = new GuessNumber();
 
 		String randomAnswer = guessNumber.generateRandomAnswer();
 		System.out.println(randomAnswer);
+	}
+
+	@Test
+	public void should_return_invalid_and_invalid_input_message() {
+		GuessNumber guessNumber = new GuessNumber();
+
+		String randomAnswer = guessNumber.generateRandomAnswer();
+		System.out.println(randomAnswer);
+
+		assertEquals("Wrong Input，Input again", guessNumber.validateInput("1134"));
+		assertEquals("Wrong Input，Input again", guessNumber.validateInput("12"));
+		assertEquals("ok", guessNumber.validateInput("1234"));
 	}
 }
